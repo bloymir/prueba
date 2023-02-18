7f726c0c4061b27698bc9cb1998cd20116381392
Nelson Tapia <ntapiapinto@gmail.com>
2023-02-18 01:49:46 -0300

    Initial Commit

---
+++ UIKit in code/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit in code
+//
+//  Created by nelson tapia on 18-02-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
