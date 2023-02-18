047ba8cd9c45697a7ce1fc8b8b66eeac56e19194
Nelson Tapia <ntapiapinto@gmail.com>
2023-02-18 02:19:50 -0300

    Eliminar Views

--- UIKit in code/Controller/UIView_Controller.swift
+++ UIKit in code/Controller/UIView_Controller.swift
@@ -9,36 +9,13 @@ import UIKit
 
 class UIView_Controller: UIViewController {
     
-    let redView: UIView = {
-        let v = UIView()
-        v.backgroundColor = .red
-        v.translatesAutoresizingMaskIntoConstraints = false
-        return v
-    }()
-    
-    let blueView: UIView = {
-        let b = UIView()
-        b.backgroundColor = .blue
-        b.translatesAutoresizingMaskIntoConstraints = false
-        return b
-    }()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
         
-        view.addSubview(redView)
-        redView.heightAnchor.constraint(equalToConstant: 200).isActive = true
-        redView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        redView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        redView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        
-        redView.addSubview(blueView)
-
-        blueView.heightAnchor.constraint(equalToConstant: 100).isActive = true
-        blueView.widthAnchor.constraint(equalToConstant: 100).isActive = true
-        blueView.centerYAnchor.constraint(equalTo: redView.centerYAnchor).isActive = true
-        blueView.centerXAnchor.constraint(equalTo: redView.centerXAnchor).isActive = true
+    
     }
 }
